9ba609d4a7a1e2a82617f89efcb72290fd6af3a7
deosanta <santanurag_deo@mail.com>
2025-03-27 10:21:10 +0530

    install_commands updated

---
+++ install_venvcommands.bat
@@ -0,0 +1,13 @@
+pip install opencv-python
+pip install openpyxl
+pip install win32com
+pip install pywin32
+pip install pandas
+pip install num2words
+pip install pyglet
+pip install pyautogui
+pip install tkcalendar
+pip install xlsxwriter
+pip install pillow
+pip install python-docx
+pip install cryptography
